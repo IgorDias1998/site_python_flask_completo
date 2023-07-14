524c4ec2953ee71f1bab6c83023f333859e7046a
Igor Dias <106784522+IgorDias1998@users.noreply.github.com>
2023-07-14 10:13:22 -0300

    Update __init__.py

--- siteCompleto/__init__.py
+++ siteCompleto/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -18,5 +19,16 @@ bcrypt = Bcrypt(app)
 loginmanager = LoginManager(app)
 loginmanager.login_view = 'login'
 loginmanager.login_message_category = 'alert-info'
+
+from siteCompleto import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        bancodedados.drop_all()
+        bancodedados.create_all()
+        print("Base de dados criada.")
+else:
+    print("Base de dados existente.")
 
 from siteCompleto import routes
