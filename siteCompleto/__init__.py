8d8c18f20ff699a775d1876fdc9daee5af3dd500
Igor Dias <106784522+IgorDias1998@users.noreply.github.com>
2023-07-19 23:33:54 -0300

    Teste__init__.py

--- siteCompleto/__init__.py
+++ siteCompleto/__init__.py
@@ -8,10 +8,8 @@ import sqlalchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '84c8d62895eb539ef7f44fe1289b3b2a'
-if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projeto.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 bancodedados = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
